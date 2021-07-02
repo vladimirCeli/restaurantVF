46c662856148bdf524e721a4afd2114badacf032
Thor-369 <juan.p.torres@unl.edu.ec>
2021-07-02 12:57:27 -0500

    squema

---
+++ models/users.js
@@ -0,0 +1,23 @@
+  
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  
+  email: String,
+  password: String,
+  name:String,
+  lastname: String
+});
+
+userSchema.methods.encryptPassword = (password) => {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+
+userSchema.methods.comparePassword= function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('user', userSchema);
