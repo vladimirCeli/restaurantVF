eae88c0bdd5c63c613ae366b902e444031245c4c
RubierAndres <rubier.padilla@unl.edu.ec>
2021-08-06 23:35:58 -0500

    Agregando lista de platillos, editar perfil y carrito de compras

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,14 +1,20 @@
-const { Router } = require("express");
-const usersCtrl = Router(); 
+const {
+  Router
+} = require("express");
+const usersCtrl = Router();
 const passport = require("passport");
 const users = require("../models/users");
 require("../passport/local-auth")(passport);
 
 usersCtrl.renderRegistrarse = (req, res, next) => {
   if (req.isAuthenticated()) {
-    res.render("index", { title: "Bienvenidos a La-Place" });
+    res.render("index", {
+      title: "Bienvenidos a La-Place"
+    });
   } else {
-    res.render("registrarse", { title: "Registrate" });
+    res.render("registrarse", {
+      title: "Registrate"
+    });
   }
 };
 
@@ -18,11 +24,15 @@ usersCtrl.registrarse = () => passport.authenticate("local-signup", {
   failureFlash: true,
 });
 
-usersCtrl.renderIngresar = (req, res,next) => {
+usersCtrl.renderIngresar = (req, res, next) => {
   if (req.isAuthenticated()) {
-    res.render("index", { title: "Login" });
+    res.render("index", {
+      title: "Login"
+    });
   } else {
-    res.render("iniciosesion", { title: "Accede" });
+    res.render("iniciosesion", {
+      title: "Accede"
+    });
     // next();
   }
 };
@@ -38,28 +48,110 @@ usersCtrl.logout = (req, res, next) => {
   res.redirect("/");
 };
 
-usersCtrl.renderEditar = (req, res) => {
-  res.render("editarp", { title: "Editar Perfil" });
-};
-usersCtrl.renderRoles = (req, res) => {
-  users.find({}, (error, users) => { 
-    res.render('asigroles', { users, title: 'Asignacion de roles', 
-    sesion: false,
-    msg: {error: req.flash('error'), info: req.flash('info')},
+//render to profile
+usersCtrl.renderProfile = (req, res) => {
+  try {
+    res.render('profile', {
+      user: req.user,
+      title: 'Perfil'
     });
-  }).sort({ timestamp: -1 });
+  } catch (error) {
+    res.render("/", {
+      error: true,
+      msg: 'Datos no válidos'
+    });
+    console.log('error', error);
+  }
+};
+
+//all edit user here
+usersCtrl.renderEditar = async (req, res) => {
+  try {
+    const existUser = await users.findOne({
+      _id: req.params.id
+    });
+    console.log(existUser, 'USER OK');
+    res.render("editarp", {
+      user: existUser,
+      error: false,
+      title: 'Editar'
+    });
+  } catch (error) {
+    res.render("profile", {
+      error: true,
+      msg: 'Datos no válidos'
+    });
+    console.log('error', error);
+  }
+};
+
+usersCtrl.renderM = (req, res)=>{
+  res.render('editarp', {
+    title: 'nuevo'
+  });
+};
+
+usersCtrl.renderEditarPut = async (req, res) => {
+  const id = req.params.id
+  const body = req.body
+
+  try {
+    const user = await users.findByIdAndUpdate(id, body, {
+      useFindAndModify: false
+    })
+    console.log(user, 'USER PUT')
+    res.json({
+      estado: true,
+      mensaje: 'Editado'
+    })
+
+  } catch (error) {
+    console.log('error', error);
+
+    res.json({
+      estado: false,
+      mensaje: 'No se completo la acción!!'
+    })
+  }
+};
+
+usersCtrl.renderRoles = (req, res) => {
+  users.find({}, (error, users) => {
+    res.render('asigroles', {
+      users,
+      title: 'Asignacion de roles',
+      sesion: false,
+      msg: {
+        error: req.flash('error'),
+        info: req.flash('info')
+      },
+    });
+  }).sort({
+    timestamp: -1
+  });
 };
 usersCtrl.editRoles = async (req, res, next) => {
-   
-  let { id } = req.params;
+
+  let {
+    id
+  } = req.params;
   const user = await users.findById(id);
-  if(user.rol==1){
-    user.rol=2;
-  }else if(user.rol==2){
-    user.rol=1;
+  if (user.rol == 1) {
+    user.rol = 2;
+  } else if (user.rol == 2) {
+    user.rol = 1;
   }
-  await user.save(); 
+  await user.save();
   res.redirect('/roles');
 };
- 
+
+//this function verify if user it's loggued
+function isLogged(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    return res.redirect('/');
+  }
+}
+
 module.exports = usersCtrl;
